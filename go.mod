fdb00226754b20def695881946042813b3083417
prakhar-kt <rahkarp@gmail.com>
2024-12-07 14:38:21 +0530

    Added bigBarks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/prakhar-kt/puppy
 
 go 1.21.3
+
+require github.com/prakhar-kt/dog v0.0.0-20241207085852-7e3ab2606b27 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,23 @@
 package puppy
 
+import (
+	"fmt"
+
+	"github.com/prakhar-kt/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
